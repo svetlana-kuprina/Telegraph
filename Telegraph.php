d51a73e0d4d71354eb955d542feab7e6dc3237ec
svetlana-kuprina <kupsveta@rambler.ru>
2023-05-08 17:04:07 +0500

    добавила коментарии

--- Telegraph.php
+++ Telegraph.php
@@ -2,47 +2,65 @@
 
 class TelegraphText
 {
-    public $text; //текст
-    public $title; //заголовок текста
-    public $author; //имя автора
-    public $published; // дата и время последнего изменения текста
-    public $slug; //уникальное имя файла, в котором будет храниться текст
+    public string $text, $title, $author, $published, $slug;
 
-    public function __construct($author, $slug)
-    {
-        $this->published=date('l jS \of F Y h:i:s A');
+    /*
+     * @param string $author
+     * @param string $slug
+     *
+     * @return void
+     */
+    public function __construct(string $author, string $slug) {
+        $this->published = date('l jS \of F Y h:i:s A');
         $this->author = $author;
         $this->slug = $slug;
     }
-public function storeText(){
-    $arrStoreText = [
-        'text'=>  $this->text,
-        'title'=> $this->title,
-        'author'=>$this->author,
-       'published' => $this->published];
-    file_put_contents($this->slug,serialize($arrStoreText));
-}
-public function loadText(){
-    if (filesize($this->slug)){
+
+    /*
+     * @return void
+     */
+    public function storeText(): void{
+        $arrStoreText = [
+            'text' =>  $this->text,
+            'title' => $this->title,
+            'author' => $this->author,
+            'published' => $this->published
+        ];
+        file_put_contents($this->slug, serialize($arrStoreText));
+    }
+
+    /*
+     * @return string
+     */
+    public function loadText(): string {
+        if (!file_exists($this->slug) || filesize($this->slug) <= 0) {
+            return '';
+        }
         $arrLoadText = unserialize(file_get_contents($this->slug));
         $this->text = $arrLoadText['text'];
         $this->title = $arrLoadText['title'];
         $this->author = $arrLoadText['author'];
         $this->published = $arrLoadText['published'];
-        }
-    return $this->text;
+        return $this->text;
     }
-public function editText(string $title, string $text){
-    $this->text = $text;
-    $this->title = $title;
-}
+
+    /*
+     * @param string $title
+     * @param string $text
+     *
+     * @return void
+     */
+    public function editText(string $title, string $text): void {
+        $this->text = $text;
+        $this->title = $title;
+    }
 }
 $author = 'Автор';
 $slug = 'test_text_file';
 
 $proba = new TelegraphText($author, $slug);
-$proba ->text = 'текст';
-$proba ->title = 'заголовок';
+$proba->text = 'текст';
+$proba->title = 'заголовок';
 
 $proba->editText('title2','text2');
 $proba->storeText();
